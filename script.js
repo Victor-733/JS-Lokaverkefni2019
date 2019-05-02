25e146d6f58cf8fe0002c73a6ef6ed57c3d7a86c
Victor-733 <victorwahid@gmail.com>
2019-05-02 16:19:59 +0000

    add images with name

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-fetch("https://apis.is/concerts") // Call the fetch function passing the url of the API as a parameter
-.then(function() {
-    // Your code for handling the data you get from the API
-    
+const main = document.getElementById("gallery");
+
+fetch("https://apis.is/concerts")
+.then(function(results) {
+    return results.json();
 })
-.catch(function() {
+.then(function(results) {
+    let concerts = [...results.results];
+    console.log(concerts);
+
+    return concerts.map(function(concert) { // Map through the results and for each run the code below
+        let div = document.createElement('div'), //  Create the elements we need
+            img = document.createElement('img'),
+            span = document.createElement('span');
+        
+        img.src = concert.imageSource;  // Add the source of the image to be the src of the img element
+        span.innerHTML = concert.eventDateName; // Make the HTML of our span to be the first and last name of our author
+        
+        div.appendChild(img);
+        div.appendChild(span);
+        main.appendChild(div);
+    });
+})
+.catch(function(error) {
     // This is where you run code if the server returns any errors
 });
