e136cb27e6ec98abdcc238c052303a0c9311fb07
Victor-733 <victorwahid@gmail.com>
2019-05-06 14:59:53 +0000

    finish setting up search feature

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const main = document.getElementById("gallery");
+let concerts;
 
 fetch("https://apis.is/concerts")
 .then(function(results) {
     return results.json();
 })
 .then(function(results) {
-    let concerts = [...results.results];
+    concerts = [...results.results];
     console.log(concerts);
 
-    return concerts.map(function(concert) { // Map through the results and for each run the code below
+    concerts = concerts.map(function(concert) { // Map through the results and for each run the code below
         let div = document.createElement('div'), //  Create the elements we need
             img = document.createElement('img'),
             p_name = document.createElement('p'),
@@ -16,7 +17,7 @@ fetch("https://apis.is/concerts")
             date = document.createElement('span');
         
         img.src = concert.imageSource;  // Add the source of the image to be the src of the img element
-        p_name.textContent = concert.eventDateName; // Make the HTML of our span to be the first and last name of our author
+        p_name.textContent = concert.eventDateName;
         p_place.textContent = concert.eventHallName;
         date.innerHTML = concert.dateOfShow;
         
@@ -25,11 +26,28 @@ fetch("https://apis.is/concerts")
         div.appendChild(p_place);
         div.appendChild(date);
         main.appendChild(div);
-    });
 
-    submit = getElementById("search-button");
-    submit.addEventListener("click", doSearch);
+        concert.element = div;
+        return concert;
+    });
+    const submit = document.getElementById("search");
+    const input_box = document.getElementById("search-bar");
+    submit.addEventListener("submit", doSearch);
+    // when the search button is pressed
+    function doSearch(e){
+        e.preventDefault();
+        console.log("hello");
+        concerts.forEach(function(concert) {
+            console.log(input_box);
+            if (concert.eventDateName.includes(input_box.value)) {
+                concert.element.removeAttribute("hidden");
+            } else {
+                concert.element.setAttribute("hidden", "");
+            }
+        });
+    }
 })
+
 .catch(function(error) {
     // This is where you run code if the server returns any errors
 });
